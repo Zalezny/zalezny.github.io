042d7f197a537269449d0373ed33b30cf173f2c3
Zalezny <zaleznypl@gmail.com>
2024-06-05 18:11:50 +0200

    Fixes margins and better animations in section Projects

--- src/Experience/ExperienceCard.tsx
+++ src/Experience/ExperienceCard.tsx
@@ -1,10 +1,14 @@
 import styles from './Experience.module.css';
 
-function ExperienceCard({ title, dateWork, description, skillsUsed }: { title: string, dateWork: string, description: string, skillsUsed: string[] }) {
+function ExperienceCard({ title, dateStart, dateEnd, description, skillsUsed }: { title: string, dateStart: string, dateEnd: string, description: string, skillsUsed: string[] }) {
     return (
         <div className={styles.container_row}>
-            <h2 className={styles.date_work}>{dateWork}</h2>
-            <div>
+            <div className={styles.container_dates}>
+            <h2 className={styles.date_work}>{dateStart}</h2>
+            <h2 className={styles.date_work}>{dateEnd}</h2>
+            </div>
+            
+            <div className={styles.container_text}>
                 <h3>{title}</h3>
                 <p className={styles.description}> {description}</p>
                 <ul className={styles.container_skills}>
--- src/Projects/ProjectCard.tsx
+++ src/Projects/ProjectCard.tsx
@@ -1,19 +1,22 @@
 
 import styles from './Projects.module.css';
 
-function ProjectCard({ srcCourrency, title, description, isNotebook }: { srcCourrency: string[], title: string, description: string, isNotebook: boolean }) {
+function ProjectCard({ srcCourrency, title, description, isNotebook, links }: { srcCourrency: string[], title: string, description: string, isNotebook: boolean, links: { btnName: string, link: string }[] }) {
     return (
         <div className={styles.project_item} >
             <div className={styles.container_img}>
                 {srcCourrency.map((src) =>
-                    <img src={'/src/assets/' + src} alt={title} style={{ maxWidth: isNotebook ? '42rem' : '14rem' }} />
+                    <div className={styles.image}> <img src={'/src/assets/' + src} alt={title} style={{ maxWidth: isNotebook ? '42rem' : '14rem' }} /></div>
                 )}
 
             </div>
             <div className={styles.container_texts} >
                 <h2 className={styles.title_project}>{title}</h2>
                 <p className={styles.description_project}>{description}</p>
-                <a href="#" className={styles.btn}>Link Button</a>
+                <div className={styles.container_btns}>
+                    {links.map((link) => <a href={link.link} className={styles.btn}>{link.btnName}</a>)}
+                </div>
+
             </div>
 
         </div>
