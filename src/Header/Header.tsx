82374ecb60d677b8836fb6486e6b424eeae17eaa
Zalezny <zaleznypl@gmail.com>
2024-06-07 08:11:50 +0200

    Added smooth scroll. Created anchor menu and fixed paddings

--- src/Header/Header.tsx
+++ src/Header/Header.tsx
@@ -1,19 +1,35 @@
+import { useEffect, useRef, useState } from 'react';
 import styles from './Header.module.css'
 import Markup from './Markup'
 
 function Header() {
+    const [isVisible, setIsVisible] = useState(false);
+    const sectionRef = useRef(null);
+    useEffect(() => {
+        const observer = new IntersectionObserver((entries) => {
+            const entry = entries[0];
+            setIsVisible(entry.isIntersecting);
+            console.log(isVisible);
+        },
+            {
+                threshold: 0.1
+            }
+        );
+        if (sectionRef.current)
+            observer.observe(sectionRef.current);
 
-    // useEffect(() => {
-    //     const observer = new IntersectionObserver((entries) => {
-    //         const entry = entries[0];
-    //     });
-    // });
+        return () => {
+            if (sectionRef.current)
+                observer.unobserve(sectionRef.current);
+        }
+    });
+
 
     return (
-        <header >
-            <nav className='navigation'>
+        <header id='header' ref={sectionRef}>
+            <nav className={isVisible ? styles.navigation : [styles.navigation, styles.sticky].join(' ')}>
                 <ul className={styles.navigation_list}>
-                    <li key='header'><a className={styles.navigation_item} href='#header'>Home</a></li>
+                    <li key='header'><a className={styles.navigation_item} href='#'>Home</a></li>
                     <li key='about'><a className={styles.navigation_item} href='#about'>About</a></li>
                     <li key='skills'><a className={styles.navigation_item} href="#skills">Skills</a></li>
                     <li key='experience'><a className={styles.navigation_item} href="#experience">Experience</a></li>
