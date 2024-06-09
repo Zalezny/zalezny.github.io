77c8145eac44c8ff2052a546e7963d19aa286648
Zalezny <zaleznypl@gmail.com>
2024-06-09 22:02:32 +0200

    Fixes for mobile and created exp, projects, contact and footer section for mobile

--- src/Projects/ProjectCard.tsx
+++ src/Projects/ProjectCard.tsx
@@ -3,9 +3,11 @@ import { useGSAP } from '@gsap/react';
 import { gsap } from 'gsap';
 import styles from './Projects.module.css';
 import { useRef } from 'react';
+import { useMediaQuery } from 'react-responsive';
 
 function ProjectCard({ srcCourrency, title, description, isNotebook, links }: { srcCourrency: string[], title: string, description: string, isNotebook: boolean, links: { btnName: string, link: string }[] }) {
     const projectItemRef = useRef(null);
+    const isMobile = useMediaQuery({ query: '(max-width: 768px)' });
     useGSAP(() => {
         const tl = gsap.timeline({
             scrollTrigger: {
@@ -16,12 +18,16 @@ function ProjectCard({ srcCourrency, title, description, isNotebook, links }: { 
         tl.from(projectItemRef.current, { x: 20, opacity: 0, duration: 1 })
     });
 
+    var imgs = srcCourrency.map((src) =>
+        <div key={src} className={styles.image}> <img src={'assets/' + src} alt={title} style={{ maxWidth: isNotebook ? '42rem' : '14rem' }} /></div>
+    );
+
+    isMobile && imgs.length > 1 ? imgs.pop() : null;
+
     return (
         <div ref={projectItemRef} className={styles.project_item} >
             <div className={styles.container_img}>
-                {srcCourrency.map((src) =>
-                    <div key={src} className={styles.image}> <img src={'assets/' + src} alt={title} style={{ maxWidth: isNotebook ? '42rem' : '14rem' }} /></div>
-                )}
+                {imgs}
 
             </div>
             <div className={styles.container_texts} >
