4c6e7371424c9fc182cbd31f722ef6210856546b
Zalezny <zaleznypl@gmail.com>
2024-06-10 16:51:38 +0200

    change label

--- src/Projects/Projects.tsx
+++ src/Projects/Projects.tsx
@@ -35,7 +35,7 @@ function Projects() {
                     ]}
                 />
                 <div className={styles.btn_more}>
-                    <a href='https://github.com/Zalezny?tab=repositories' className={styles.btn_more_text}>More projects</a>
+                    <a href='https://github.com/Zalezny?tab=repositories' className={styles.btn_more_text}>View More</a>
                     <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="#0161ef" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"><g fill="none" fill-rule="evenodd"><path d="M18 14v5a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V8c0-1.1.9-2 2-2h5M15 3h6v6M10 14L20.2 3.8" /></g></svg>
                 </div>
             </div>
