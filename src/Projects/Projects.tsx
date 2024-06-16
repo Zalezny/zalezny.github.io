0bd445d21fab1230a1db090b4cebe6c717a852f8
Zalezny <zaleznypl@gmail.com>
2024-06-16 18:30:54 +0200

    change project files on webp

--- src/Projects/Projects.tsx
+++ src/Projects/Projects.tsx
@@ -8,29 +8,29 @@ function Projects() {
         <section id='projects' className={styles.projects}>
             <h1 className={[appStyles.heading, styles.heading].join(' ')}>{t('Projects')}</h1>
             <div className={styles.container_projects}>
-                <ProjectCard srcCourrency={['currencyapp.png', 'currencyapp1.png', 'currencyapp2.png']} title='Currency App' isNotebook={false} description={t('CurrencyAppDescription')}
+                <ProjectCard srcCourrency={['currencyapp.webp', 'currencyapp1.webp', 'currencyapp2.webp']} title='Currency App' isNotebook={false} description={t('CurrencyAppDescription')}
                     links={[
                         { btnName: t('Code'), link: 'https://github.com/Zalezny/currency_app' },
                     ]}
                 />
-                <ProjectCard srcCourrency={['outfitflutter.png', 'outfitflutter1.png', 'outfitflutter2.png']} title='Katya Outfit' isNotebook={false} description={t('KatyaOutfitDesc')}
+                <ProjectCard srcCourrency={['outfitflutter.webp', 'outfitflutter1.webp', 'outfitflutter2.webp']} title='Katya Outfit' isNotebook={false} description={t('KatyaOutfitDesc')}
                     links={[
                         { btnName: 'Flutter ' + t('Code'), link: 'https://github.com/Zalezny/outfit_flutter' },
                         { btnName: 'Kotlin ' + t('Code'), link: 'https://github.com/Zalezny/OutfitKatyas' },
                     ]}
                 />
-                <ProjectCard srcCourrency={['ordersflutter.png', 'ordersflutter1.png', 'ordersflutter2.png']} title='Katya Orders' isNotebook={false} description={t('KatyaOrdersDesc')}
+                <ProjectCard srcCourrency={['ordersflutter.webp', 'ordersflutter1.webp', 'ordersflutter2.webp']} title='Katya Orders' isNotebook={false} description={t('KatyaOrdersDesc')}
                     links={[
                         { btnName: 'Flutter ' + t('Code'), link: 'https://github.com/Zalezny/orders_flutter' },
                         { btnName: 'Kotlin ' + t('Code'), link: 'https://github.com/Zalezny/OrderKatyas' },
                     ]}
                 />
-                <ProjectCard srcCourrency={['beautyplaces.png', 'beautyplaces1.png', 'beautyplaces2.png']} title='Beauty Places' isNotebook={false} description={t('BeautyPlacesDesc')}
+                <ProjectCard srcCourrency={['beautyplaces.webp', 'beautyplaces1.webp', 'beautyplaces2.webp']} title='Beauty Places' isNotebook={false} description={t('BeautyPlacesDesc')}
                     links={[
                         { btnName: t('Code'), link: 'https://github.com/Zalezny/BeautyPlaces' },
                     ]}
                 />
-                <ProjectCard srcCourrency={['flappybird.png']} title='FlappyBird' isNotebook={true} description={t('FlappyBirdDesc')}
+                <ProjectCard srcCourrency={['flappybird.webp']} title='FlappyBird' isNotebook={true} description={t('FlappyBirdDesc')}
                     links={[
                         { btnName: t('Code'), link: 'https://github.com/Zalezny/FlappyBird' },
                     ]}
